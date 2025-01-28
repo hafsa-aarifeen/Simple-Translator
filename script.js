e09816cc77159a4daeeac404b9d5b854e832d5c6
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2025-01-28 12:54:32 +0530

    Creating Options Tag & Adding it inside the Select Tag

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ import countries from './countries.js';
 
 const selectTag = document.querySelectorAll("select");
 
-selectTag.forEach(tag => {
-    for (const country_code in countries) {
-        console.log(country_code);
+selectTag.forEach((tag, id) => {
+    for (let country_code in countries) {
+        let selected;
+        if(id == 0 && country_code == "en-GB") {
+            selected = "selected";
+        } else if(id == 1 && country_code == "hi-IN") {
+            selected = "selected";
+        }
+        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
     }
 });
