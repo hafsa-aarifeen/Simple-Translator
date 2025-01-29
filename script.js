6c14e850e41b0dda4e00249e726a6d6d5420d7f6
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2025-01-29 18:17:52 +0530

    Exchange Icon

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import countries from './countries.js';
 const fromText = document.querySelector(".from-text");
 const toText = document.querySelector(".to-text");
 const selectTag = document.querySelectorAll("select");
+const exchageIcon = document.querySelector(".exchange");
 const translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag, id) => {
@@ -11,6 +12,15 @@ selectTag.forEach((tag, id) => {
         let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option);
     }
+});
+
+exchageIcon.addEventListener("click", () => {
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    toText.value = tempText;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang;
 });
 
 translateBtn.addEventListener("click", () => {
