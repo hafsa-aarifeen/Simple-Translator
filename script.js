c9bce8da778ea56bc4f0894f2ecd3e36d3f17ff9
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2025-01-29 09:44:33 +0530

    Sending Request to API & Fetching Data of Entered Text From API

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 import countries from './countries.js';
 
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
 const selectTag = document.querySelectorAll("select");
+const translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag, id) => {
     for (let country_code in countries) {
-        let selected;
-        if(id == 0 && country_code == "en-GB") {
-            selected = "selected";
-        } else if(id == 1 && country_code == "hi-IN") {
-            selected = "selected";
-        }
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
         let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option);
     }
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value.trim(),
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    if(!text) return;
+    toText.setAttribute("placeholder", "Translating...");
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        console.log(data);
+        toText.value = data.responseData.translatedText;
+    });
 });
